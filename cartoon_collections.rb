7e5c55ba8453d53362b3e8eeb27797e847983c7d
Jessy Hu <jesshuy12@gmail.com>
2019-02-02 00:14:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese_types|
-    cheese_types 
+    cheese_types.include?(cheese_types)
   end
 end
